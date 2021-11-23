c866877d7674e959d1d817d2b399f52e571a3375
Ville.Kähärä <kahara.ville@gmail.com>
2021-11-23 11:43:32 +0200

    Feedback count sum and averages

--- task1/unicafe/src/App.js
+++ task1/unicafe/src/App.js
@@ -33,6 +33,15 @@ const App = () => {
             <p> Good: {good}</p>
             <p> Neutral: {neutral}</p>
             <p> Bad: {bad}</p>
+            <p> All: {good + neutral + bad}</p>
+            <h3> Feedback average</h3>
+            {/* As note! I'm not certain what task instructions mean by this weighted average. Would be needed to be reseacrhed more
+                As I intercepted this weighted task was about how to use variables and math operations in jsx files not so much as of the task in hand.
+                As such to see the reactivity of the feedback buttons corresponding to the statistics
+                But to continue this excercise I left these averages as a placeholder. Would be needed to fix if shipped to production.
+            */}
+            <p> Weighted Average: {((good * 1) + (neutral * 0) + (bad * -1) / (good + neutral + bad) )}</p>
+            <p> Positive Average: {(good / (good + neutral + bad) * 100 )} %</p>
           </div>
         </section>
       </main>

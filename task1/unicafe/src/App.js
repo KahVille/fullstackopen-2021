60272cf02b63e597c6817d146865b327fbbeda06
Ville.Kähärä <kahara.ville@gmail.com>
2021-11-23 12:54:42 +0200

    Feedbackstatistics as a html table

--- task1/unicafe/src/App.js
+++ task1/unicafe/src/App.js
@@ -1,40 +1,40 @@
 import { useState } from "react";
 
-
-const FeedbackStatisticLine = ({text, feedbackValue}) => {
+const FeedbackStatisticLine = ({ text, feedbackValue }) => {
   return (
-    <p>{text}: {feedbackValue}</p>
+    <tr><td>{text}</td><td>{feedbackValue}</td></tr>
   );
 }
-
 
 const FeedbackButton = (props) => {
   return (
-  <button onClick={props.handleFeedback}>{props.text}</button>
+    <button onClick={props.handleFeedback}>{props.text}</button>
   );
 
 }
 
-const Statistics = ({good, neutral, bad}) => {
+const Statistics = ({ good, neutral, bad }) => {
   return (
     <section className="user-feedback-statistics">
-    <h2>Statistics</h2>
-    <div className="feedback-statistics">
-      <h3>Feedback count</h3>
-      <FeedbackStatisticLine text="Good" feedbackValue={good} />
-      <FeedbackStatisticLine text="Neutral" feedbackValue={neutral} />
-      <FeedbackStatisticLine text="Bad" feedbackValue={bad} />
-      <FeedbackStatisticLine text="All" feedbackValue={good + neutral + bad} />
-      <h3> Feedback average</h3>
-      {/* As note! I'm not certain what task instructions mean by this weighted average. Would be needed to be reseacrhed more
+      <h2>Statistics</h2>
+      <div className="feedback-statistics">
+        <table>
+          <tbody>
+            <FeedbackStatisticLine text="Good" feedbackValue={good} />
+            <FeedbackStatisticLine text="Neutral" feedbackValue={neutral} />
+            <FeedbackStatisticLine text="Bad" feedbackValue={bad} />
+            <FeedbackStatisticLine text="All" feedbackValue={good + neutral + bad} />
+            {/* As note! I'm not certain what task instructions mean by this weighted average. Would be needed to be reseacrhed more
           As I intercepted this weighted task was about how to use variables and math operations in jsx files not so much as of the task in hand.
           As such to see the reactivity of the feedback buttons corresponding to the statistics
           But to continue this excercise I left these averages as a placeholder. Would be needed to fix if shipped to production.
       */}
-      <FeedbackStatisticLine text="Weighted Average" feedbackValue={((good * 1) + (neutral * 0) + (bad * -1) / (good + neutral + bad) )} />
-      <FeedbackStatisticLine text=" Positive Average" feedbackValue={ (good / (good + neutral + bad) * 100 ) + ' %'} />
-    </div>
-  </section>
+            <FeedbackStatisticLine text="Weighted Average" feedbackValue={((good * 1) + (neutral * 0) + (bad * -1) / (good + neutral + bad))} />
+            <FeedbackStatisticLine text=" Positive Average" feedbackValue={(good / (good + neutral + bad) * 100) + ' %'} />
+          </tbody>
+        </table>
+      </div>
+    </section>
 
   );
 }
@@ -70,9 +70,9 @@ const App = () => {
             Provide feedback on overall experience
           </h2>
           <div className="feedback-buttons">
-            <FeedbackButton handleFeedback={() => setGoodValue(good+1)} text="Good"/>
-            <FeedbackButton handleFeedback={() => setNeutralValue(neutral+1)} text="Neutral"/>
-            <FeedbackButton handleFeedback={() => setBadValue(bad+1)} text="Bad"/>
+            <FeedbackButton handleFeedback={() => setGoodValue(good + 1)} text="Good" />
+            <FeedbackButton handleFeedback={() => setNeutralValue(neutral + 1)} text="Neutral" />
+            <FeedbackButton handleFeedback={() => setBadValue(bad + 1)} text="Bad" />
           </div>
         </section>
 

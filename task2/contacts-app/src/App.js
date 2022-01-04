1140a72517c70bfae6c8a39c01cb14d37ced1aa1
Ville Kähärä <kahara.ville@gmail.com>
2022-01-04 11:48:46 +0200

    Add new name to contacts

--- task2/contacts-app/src/App.js
+++ task2/contacts-app/src/App.js
@@ -7,12 +7,46 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+
+  const handleContactAdd = (event) => {
+    event.preventDefault();
+
+    const newContact = {
+        name: newName
+    };
+
+    if(!IsNewContactValid(persons,newContact))
+    {
+      alert(`${newName} is already added to contacts`);
+      console.warn(`${newName} is already added to contacts`);
+      return;
+    }
+
+    setPersons([...persons,newContact]);
+    setNewName('');
+
+  }
+
+  const handleContactChange = (event) => {
+  setNewName(event.target.value)
+  }
+
+  const IsNewContactValid = (persons, newContact) => {
+    let personFound = persons.filter((person) => person.name === newContact.name);
+    return personFound.length > 0 ? false : true;
+  }
+
+
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={(event) => handleContactAdd(event)}>
         <div>
-          name: <input />
+          name: <input
+           value={newName}
+           onChange={(event) => handleContactChange(event)}
+          />
         </div>
         <div>
           <button type="submit">add</button>

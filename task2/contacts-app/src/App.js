f230493d710c6aa4789aff8a04cf9c20f1a32191
Ville Kähärä <kahara.ville@gmail.com>
2022-01-04 11:00:55 +0200

    Display contact name on a list

--- task2/contacts-app/src/App.js
+++ task2/contacts-app/src/App.js
@@ -1,3 +1,6 @@
+import { useState } from "react"
+
+// Conntacts App
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
@@ -16,7 +19,11 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+      {persons.map((person) => {
+        return <p key={person.name}>{person.name}</p>
+      })}
+      </div>
     </div>
   )
 

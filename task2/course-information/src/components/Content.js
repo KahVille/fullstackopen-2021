ddc1fbb349d5c5162fb48fb55f04f04c5c8ac189
Ville Kähärä <kahara.ville@gmail.com>
2022-01-04 09:38:07 +0200

    Fixed: Course rendering from static to dynamic

--- task2/course-information/src/components/Content.js
+++ task2/course-information/src/components/Content.js
@@ -3,10 +3,11 @@ import Part from "./Part";
 //Course content
 const Content = ({parts}) => {
     return(
+
      <div>
-        <Part part={parts[0]} />
-        <Part part={parts[1]} />
-        <Part part={parts[2]} />
+        {parts.map((part => {
+            return <Part key={part.id} part={part}/>
+        }))}
      </div>
     );
 }

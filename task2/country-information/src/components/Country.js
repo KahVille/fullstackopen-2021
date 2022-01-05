a214a522a33bc7edc7b40ffa3de14cfa66826b1d
Ville Kähärä <kahara.ville@gmail.com>
2022-01-05 18:04:42 +0200

    Country details view

---
+++ task2/country-information/src/components/Country.js
@@ -0,0 +1,23 @@
+// Country details
+const Country = ({country}) => {
+    
+    const name = country.name.common;
+    const [capital] = country.capital;
+    const region = country.region;
+    const flag = country.flag;
+
+
+    return <div>
+    <h2>{name}</h2>
+    <p>{capital}</p>
+    <p>{region}</p>
+
+    <h3>Languages</h3>    
+    {Object.entries(country.languages).map(([key, value]) => {return <p key={key}>{value}</p>})}
+
+    <p>{flag}</p>
+
+    </div>
+}
+
+export default Country;

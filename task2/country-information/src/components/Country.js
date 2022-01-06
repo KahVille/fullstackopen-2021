6e6bd9dfa49d36a931c2cdc02070c3f9f7e83adb
Ville Kähärä <kahara.ville@gmail.com>
2022-01-06 17:34:49 +0200

    Added fake weather info instead of info from api

--- task2/country-information/src/components/Country.js
+++ task2/country-information/src/components/Country.js
@@ -1,13 +1,18 @@
+import { useEffect, useState } from "react";
+
 // Country details
 const Country = ({country}) => {
 
-    if(!country)
-        return <div></div>
-    
     const name = country.name.common;
     const [capital] = country.capital;
     const region = country.region;
     const flag = country.flag;
+
+    const [weatherDetails, setWeatherDetails] = useState({temperatureC : 0});
+
+    useEffect(() => {
+        setWeatherDetails({temperatureC: -10.5})
+    }, [])
 
 
     return <div>
@@ -19,6 +24,9 @@ const Country = ({country}) => {
     {Object.entries(country.languages).map(([key, value]) => {return <p key={key}>{value}</p>})}
 
     <p>{flag}</p>
+
+    <h2>Weather in {name}</h2>
+    <p>{weatherDetails.temperatureC}</p>
 
     </div>
 }

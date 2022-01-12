27f6ca1a3a85af3acfaab11180693e5d40de19a9
Ville.Kähärä <kahara.ville@gmail.com>
2022-01-12 19:00:55 +0200

    Added morgan to log request body

--- task3/contactsapp/contacts-app-api/index.js
+++ task3/contactsapp/contacts-app-api/index.js
@@ -3,9 +3,13 @@
 const express = require('express');
 const morgan = require('morgan');
 
+morgan.token('body', req => {
+  return JSON.stringify(req.body)
+})
+
 const app = express();
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const hostname = '127.0.0.1';
 const port = 3001;

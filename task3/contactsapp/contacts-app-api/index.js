1aac724f4b42efb5c1f1b9bb000143a8e43f8757
Ville.Kähärä <kahara.ville@gmail.com>
2022-01-12 16:23:42 +0200

    converted api to express app. Added fake persons data and endpoint.

--- task3/contactsapp/contacts-app-api/index.js
+++ task3/contactsapp/contacts-app-api/index.js
@@ -1,16 +1,43 @@
 // Backend api server
 
-const http = require('http');
+const express = require('express');
+const app = express();
 
 const hostname = '127.0.0.1';
 const port = 3001;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+let persons =  [
+      {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+      },
+      {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+      },
+      {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+      }
+    ];
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>');
 });
 
-server.listen(port, hostname, () => {
+app.get('/persons', (req, res) => {
+    res.json(persons);
+});
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });

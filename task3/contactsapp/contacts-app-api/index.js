887a1c9ec0c72e849fa8cd8dee711aec24173a1b
Ville.Kähärä <kahara.ville@gmail.com>
2022-01-12 15:51:22 +0200

    adding basic template for api server

--- task3/contactsapp/contacts-app-api/index.js
+++ task3/contactsapp/contacts-app-api/index.js
@@ -1,4 +1,16 @@
-// Backend api entry point
+// Backend api server
 
-console.log('Server starting...');
-console.log('Server started');
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3001;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});

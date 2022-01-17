6c3a6e3067b046b548aa4014e65bc715f649abf8
Ville.Kähärä <kahara.ville@gmail.com>
2022-01-17 10:00:06 +0200

    Add mongoose dataaccess

---
+++ task3/contactsapp/contacts-app-api/dataAccess.js
@@ -0,0 +1,25 @@
+const { mongoDBUrl } = require('./config');
+const connectionString = mongoDBUrl;
+
+const {ContactModel} = require('./Models/Contact');
+
+const mongoose = require('mongoose');
+mongoose.connect(connectionString);
+
+const getAll = async () => {
+   const promise = await ContactModel.find({});
+    return promise;
+}
+
+const addNew = async (name, number) => {
+    mongoose.connect(connectionString);
+    const contact = new ContactModel({
+        name: name,
+        number: number
+    })
+
+    const promise = await contact.save();
+    return promise;
+};
+
+module.exports = {getAll, addNew}

a5e2fa9340dc542062d32f72f3043088b760b7ee
Ville.Kähärä <kahara.ville@gmail.com>
2022-01-17 10:51:53 +0200

    removed duplicate check. add new contact cleanup

--- task3/contactsapp/contacts-app-api/index.js
+++ task3/contactsapp/contacts-app-api/index.js
@@ -96,23 +96,13 @@ app.post(`${basePersonApiPath}`,async (req, res) => {
 
     if(!newContactData.number || !newContactData.name)
       return res.status(400).json({message: 'Name or Number is missing'});
-
-    let isPersonDuplicate = false;
-    let personFound = persons.filter((person) => person.name.toLowerCase() === newContactData.name.toLowerCase());
-    isPersonDuplicate = personFound.length > 0 ? true : false;
-
-    if(isPersonDuplicate)
-      return res.status(400).json({message: `Contact is already added with the same name of ${newContactData.name}`});
-
+      
   const newContact = {
     name: newContactData.name,
     number: `${newContactData.number}`,
   };
 
   const newPerson = await addNew(newContact.name,newContact.number)
-
-  persons = [...persons, newPerson];
-
   return res.status(201).json(newPerson);
 
 });

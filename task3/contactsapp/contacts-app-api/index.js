b61b9bbd443e5f26b90267c6822790594ae053be
Ville.Kähärä <kahara.ville@gmail.com>
2022-01-17 11:47:20 +0200

    Add error handling to api middleware

--- task3/contactsapp/contacts-app-api/index.js
+++ task3/contactsapp/contacts-app-api/index.js
@@ -19,75 +19,62 @@ const hostname = '0.0.0.0';
 const port = process.env.PORT || 3001;
 const basePersonApiPath = '/api/persons';
 
-let persons =  [
-      {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-      },
-      {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-      }
-    ];
-
-
 app.get('/', (req, res) => {
   return res.send('<h1>Hello World</h1>');
 });
 
 // Contact list
 app.get(basePersonApiPath, async (req, res) => {
+  try {
     const persons = await getAll();
-    return res.json(persons);
+    return res.json(persons);    
+  } catch (error) {
+    return next(error);
+  }
+
 });
 
 // Single contact info
-app.get(`${basePersonApiPath}/:id`, async (req, res) => {
+app.get(`${basePersonApiPath}/:id`, async (req, res, next) => {
   const contactId = req.params.id;
 
    if(!contactId)
     return res.sendStatus(400);
 
-  const contact = await getSingle(contactId)
+  try {
+      const contact = await getSingle(contactId)
+      if(!contact)
+      return res.sendStatus(404)
 
-  if(!contact)
-    return res.sendStatus(404)
-
-    return res.status(200).json(contact);
+      return res.status(200).json(contact);
+  } catch (error) {
+      return next(error);    
+  }
 
 });
 
 // Remove single contact from contacts
-app.delete(`${basePersonApiPath}/:id`, async (req, res) => {
+app.delete(`${basePersonApiPath}/:id`, async (req, res, next) => {
   const contactId = req.params.id;
 
   if(!contactId)
    return res.sendStatus(400);
 
-   const contact = await getSingle(contactId)
+   try {
+    const contact = await getSingle(contactId)
 
-   if(!contact)
-     return res.sendStatus(404)
-
-  const removedContact = await removeSingle(contactId);
-  return res.status(200).json(removedContact.id);
-
+    if(!contact)
+      return res.sendStatus(404)
+ 
+   const removedContact = await removeSingle(contactId);
+   return res.status(200).json(removedContact.id);
+   } catch (error) {
+    return next(error);
+   }
 });
 
 // Create new contact
-app.post(`${basePersonApiPath}`,async (req, res) => {
+app.post(`${basePersonApiPath}`,async (req, res, next) => {
 
   const newContactData = JSON.parse(JSON.stringify(req.body));
 
@@ -96,28 +83,38 @@ app.post(`${basePersonApiPath}`,async (req, res) => {
 
     if(!newContactData.number || !newContactData.name)
       return res.status(400).json({message: 'Name or Number is missing'});
-      
+
   const newContact = {
     name: newContactData.name,
     number: `${newContactData.number}`,
   };
 
-  const newPerson = await addNew(newContact.name,newContact.number)
-  return res.status(201).json(newPerson);
+  try {
+    const newPerson = await addNew(newContact.name,newContact.number)
+    return res.status(201).json(newPerson);    
+  } catch (error) {
+    return next(error);
+  }
 
 });
 
 
 // Api basic info
-app.get('/info', (req, res) => {
-  let requestTimeAsString = new Date().toString();
-  let contactApiInfoAsString = 
-  `<div> 
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${requestTimeAsString}</p>
-  </div>`
+app.get('/info', async (req, res) => {
+  try {
+    let requestTimeAsString = new Date().toString();
+    const persons = await getAll();
 
-  return res.send(contactApiInfoAsString)
+    let contactApiInfoAsString = 
+    `<div> 
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${requestTimeAsString}</p>
+    </div>`
+  
+    return res.send(contactApiInfoAsString)    
+  } catch (error) {
+    return next(error);
+  }
 
 });
 
@@ -125,6 +122,17 @@ const unknownEndpoint = (req, res) => {
   res.status(404).send({ message: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request,response,next) => {
+  console.error(error.message);
+
+  if(error.name === 'CastError')
+  return response.status(400).send({ message: 'malformatted id'})
+  
+  next(error);
+
+};
+app.use(errorHandler);
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);

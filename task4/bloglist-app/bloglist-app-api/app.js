3d04a90b0ffa290da969201c8d849546bc2923be
Ville.Kähärä <kahara.ville@gmail.com>
2022-01-20 22:26:45 +0200

    use of middleware and logger

--- task4/bloglist-app/bloglist-app-api/app.js
+++ task4/bloglist-app/bloglist-app-api/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const { databaseUrl } = require('./config');
 const { blogsRouter } = require('./controllers/blogs');
-
+const middleware = require('./utils/middleware');
 const mongoUrl = databaseUrl;
 mongoose.connect(mongoUrl);
 
@@ -12,5 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/blogs', blogsRouter);
+
+app.use(middleware.uknownEndPoint);
+app.use(middleware.errorHandler);
 
 module.exports = { app };

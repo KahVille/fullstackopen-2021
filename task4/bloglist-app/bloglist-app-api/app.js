be4d84913cef1695c2e0dc968ebf46c3dbef1853
Ville.Kähärä <kahara.ville@gmail.com>
2022-01-20 12:30:53 +0200

    Refactor api endpoints to a controller module

--- task4/bloglist-app/bloglist-app-api/app.js
+++ task4/bloglist-app/bloglist-app-api/app.js
@@ -3,31 +3,15 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const { databaseUrl } = require('./config');
-const Blog = require('./models/blog');
-
-const app = express();
-app.use(cors());
-app.use(express.json());
+const {blogsRouter} = require('./controllers/blogs');
 
 const mongoUrl = databaseUrl;
 mongoose.connect(mongoUrl);
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then((blogs) => {
-      response.json(blogs);
-    });
-});
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api/blogs', blogsRouter);
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    });
-});
 
 module.exports = { app };

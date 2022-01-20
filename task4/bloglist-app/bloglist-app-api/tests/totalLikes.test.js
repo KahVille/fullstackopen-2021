7cb1609245c51727e82b83bd84eaa5438dad545c
Ville.Kähärä <kahara.ville@gmail.com>
2022-01-20 13:55:34 +0200

    Add test of ttal likes with multiple blogs

--- task4/bloglist-app/bloglist-app-api/tests/totalLikes.test.js
+++ task4/bloglist-app/bloglist-app-api/tests/totalLikes.test.js
@@ -22,5 +22,51 @@ describe('total likes', () => {
         const result = listHelper.totalLikes(listWithOneBlog);
         expect(result).toBe(5);
     });
+
+    const listWithMultipleBlogs = [
+        {
+            id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+        },
+        {
+            id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 3,
+        },
+        {
+            id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 0,
+        },
+        {
+            id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 9,
+        },
+        {
+            id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 13,
+        }
+    ];
+
+
+    test('of a bigger list is calculated right', () => {
+        const result = listHelper.totalLikes(listWithMultipleBlogs);
+        let expectedOutput = 5+3+0+9+13;
+        expect(result).toBe(expectedOutput);
+    });
+
 });
 

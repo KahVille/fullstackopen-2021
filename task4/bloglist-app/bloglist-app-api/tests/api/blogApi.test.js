8aaa911fd54b198bcacb653962d8cb779bf0da49
Ville.Kähärä <kahara.ville@gmail.com>
2022-01-21 14:33:54 +0200

    'post a new valid blog and check length test

--- task4/bloglist-app/bloglist-app-api/tests/api/blogApi.test.js
+++ task4/bloglist-app/bloglist-app-api/tests/api/blogApi.test.js
@@ -8,19 +8,35 @@ const apptest = supertest(app);
 describe('blogs api route', () => {
     
     test('get all blogs', async () => {
-        try {
-            await apptest.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/);            
-        } catch (error) {
-            console.log(error);
-        }
+            await apptest.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/);
     });
 
-    afterAll(async () => {
-        try {
-            await mongoose.connection.close();
-        } catch (error) {
-            console.log(error);
+    test('post a new valid blog and check length', async () => {
+        const responseStart = await apptest.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/);
+        const blogsStart = responseStart.body.map(blog => blog);
+
+        const testBlog = {
+            title: 'teest blog',
+            author: 'Matti Meikäläinen',
+            url: 'test-blog',
+            likes: 13
         }
+
+        const response = await apptest
+        .post('/api/blogs')
+        .send(testBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
+
+
+        const responseEnd = await apptest.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/);
+        const blogsEnd = responseEnd.body.map(blog => blog);
+
+        expect(blogsEnd).toHaveLength(blogsStart.length + 1);
+});
+
+    afterAll(() => {
+        mongoose.connection.close();
     });
 });
 

79684da92c1dc3a09fea2cd407192f507625f728
Ville.Kähärä <kahara.ville@gmail.com>
2022-01-22 11:47:47 +0200

    populate users and blogs

--- task4/bloglist-app/bloglist-app-api/controllers/blogs.js
+++ task4/bloglist-app/bloglist-app-api/controllers/blogs.js
@@ -4,7 +4,7 @@ const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response, next) => {
   try {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', {username: 1, name: 1});
     return response.json(blogs);
   }
   catch(error) {
--- task4/bloglist-app/bloglist-app-api/controllers/users.js
+++ task4/bloglist-app/bloglist-app-api/controllers/users.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 
 usersRouter.get('/', async (request, response, next) => {
     try {
-        const users = await User.find({});
+        const users = await User.find({}).populate('blogs', {title: 1, author:1, url: 1});       
         return response.json(users.map(user => user.toJSON()));
     } catch (error) {
         return next(error);

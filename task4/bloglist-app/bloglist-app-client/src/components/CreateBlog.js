e7a2836474904772a7f4cd86345e21321331247c
Ville.Kähärä <kahara.ville@gmail.com>
2022-01-28 18:00:27 +0200

    Move Create new blog into Toggable component

--- task4/bloglist-app/bloglist-app-client/src/components/CreateBlog.js
+++ task4/bloglist-app/bloglist-app-client/src/components/CreateBlog.js
@@ -1,30 +1,20 @@
-import { useState } from "react";
 import CreateBlogForm from "./CreateBlogForm";
-
+import Toggable from "./Toggable";
 
 const CreateBlog = ({handleCreateBlog, handleTitleChange, handleAuthorChange, handleUrlChange}) => {
 
-    const [createBlogVisible, setCreateBlogVisible] = useState(false);
-
-    const hideWhenVisible = {display: createBlogVisible ? 'none' : ''}
-    const showWhenVisible = {display: createBlogVisible ? '' : 'none'}
-    
     return ( 
     <div>
         <h2>Create a new blog</h2>
 
-        <div style={hideWhenVisible}>
-            <button onClick={() => setCreateBlogVisible(true)}>Create new blog</button>
-        </div>
-
-        <div style={showWhenVisible}>
-        <CreateBlogForm handleSubmit = {(event) => handleCreateBlog(event)} 
-            handleTitleChange={(event) => handleTitleChange(event)} 
-            handleAuthorChange ={(event) => handleAuthorChange(event)} 
-            handleUrlChange = {(event) => handleUrlChange(event)}
-            />
-            <button onClick={() => setCreateBlogVisible(false)}>Close</button>
-        </div>
+        <Toggable buttonLabel="Create new blog post">
+            <h3>Add the following Blog details</h3>
+            <CreateBlogForm handleSubmit = {(event) => handleCreateBlog(event)} 
+                handleTitleChange={(event) => handleTitleChange(event)} 
+                handleAuthorChange ={(event) => handleAuthorChange(event)} 
+                handleUrlChange = {(event) => handleUrlChange(event)}
+                />
+        </Toggable>
 
     </div>
     )

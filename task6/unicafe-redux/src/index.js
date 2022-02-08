e5931b38cf7d8406e2b86116a33ea9e0ec96634f
Ville.Kähärä <kahara.ville@gmail.com>
2022-02-08 13:14:40 +0200

    unicafe redux app starting point

--- task6/unicafe-redux/src/index.js
+++ task6/unicafe-redux/src/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore } from 'redux';
+import reducer from './reducer';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = createStore(reducer)
+
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: 'GOOD'
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={good}>good</button> 
+      <button>ok</button> 
+      <button>bad</button>
+      <button>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok</div>
+      <div>bad</div>
+    </div>
+  )
+}
+
+const renderApp = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+};
+
+renderApp()
+store.subscribe(renderApp)

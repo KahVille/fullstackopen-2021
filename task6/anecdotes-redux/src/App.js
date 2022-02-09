6af7553482887aea2694bd15d5295482655585d6
Ville.Kähärä <kahara.ville@gmail.com>
2022-02-09 14:16:52 +0200

    Add new anecdote via form

--- task6/anecdotes-redux/src/App.js
+++ task6/anecdotes-redux/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { voteAnecdoteAction } from './reducers/anecdoteReducer';
+import { voteAnecdoteAction, addAnecdoteAction } from './reducers/anecdoteReducer';
 
 const App = () => {
   const anecdotes = useSelector(state => state.anecdotes);
   const dispatch = useDispatch();
 
   const vote = (id) => dispatch(voteAnecdoteAction(id));
+
+  const addAnecdote = (event) => {
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    dispatch(addAnecdoteAction(content));
+  };
 
   return (
     <div>
@@ -24,9 +30,9 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
+      <form onSubmit={(event) => addAnecdote(event)}>
+        <div><input name='anecdote'/></div>
+        <button type='submit'>create</button>
       </form>
     </div>
   )

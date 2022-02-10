da633a1f0d9b86119cf6a3a032e0d9d82ca2a543
Ville.Kähärä <kahara.ville@gmail.com>
2022-02-10 15:30:01 +0200

    init anecdotes reducer

--- task6/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ task6/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -1,12 +1,3 @@
-const anecdotesAtStart = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-  ];
-  
   const getId = () => (100000 * Math.random()).toFixed(0);
   
   const asObject = (anecdote) => {
@@ -17,9 +8,9 @@ const anecdotesAtStart = [
     }
   };
   
-  const initialState = { anecdotes: anecdotesAtStart.map(asObject) };
+const initialState = { anecdotes: [] };
 
-  const reducer = (state = initialState, action) => {
+  const anecdoteReducer = (state = initialState, action) => {
 
     switch (action.type) {
       case 'VOTE_ANECDOTE':
@@ -28,6 +19,11 @@ const anecdotesAtStart = [
         return {
           ...state,
           anecdotes: [...state.anecdotes, asObject(action.data.content)]
+        };
+        case 'INIT_ANECDOTES':
+        return {
+          ...state,
+          anecdotes: action.anecdotes
         }
       default:
         return state;
@@ -52,6 +48,15 @@ const anecdotesAtStart = [
           content: content
       }
     }
-}
+  }
+
+  export const initAnecdotesAction = (anecdotes) => {
+    return {
+      type: 'INIT_ANECDOTES',
+      payload: 'init anecdotes from the server',
+      anecdotes: anecdotes
+    }
+  }
+
   
-  export default reducer;
+export default anecdoteReducer;

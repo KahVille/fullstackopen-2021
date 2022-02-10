20fbd860b7771669dd8ec9af20f5301d64bfe86e
Ville.Kähärä <kahara.ville@gmail.com>
2022-02-10 12:03:45 +0200

    Display app start notification

--- task6/anecdotes-redux/src/App.js
+++ task6/anecdotes-redux/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
+import Notification from './components/Notification';
 
 const App = () => {
   return (
     <div>
       <h2>Anecdotes App</h2>
-
+      <Notification />
       <AnecdoteForm />
       <AnecdoteList />
     </div>

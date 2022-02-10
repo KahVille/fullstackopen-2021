20b2dc89dfda4360ff446e9b5c3fa862bf94e892
Ville.Kähärä <kahara.ville@gmail.com>
2022-02-10 14:59:29 +0200

    Add nit anecdotes tests

--- task6/anecdotes-redux/src/reducers/anecdoteReducer.unit.test.js
+++ task6/anecdotes-redux/src/reducers/anecdoteReducer.unit.test.js
@@ -1,15 +1,14 @@
 import deepFreeze from 'deep-lock';
-import anecdoteReducer, { addAnecdoteAction, voteAnecdoteAction } from './anecdoteReducer';
+import anecdoteReducer, { addAnecdoteAction, voteAnecdoteAction, initAnecdotesAction } from './anecdoteReducer';
 
 describe('anecdote reducer', () => {
 
-    const initialState = {anecdotes: [{
-        id: 0,
-        content: 'test anecdote',
-        votes: 0
-    }]}
-
     test('anecdote voted success', () => {
+        const initialState = {anecdotes: [{
+            id: 0,
+            content: 'test anecdote',
+            votes: 0
+        }]}
         const action = {
             type: 'VOTE_ANECDOTE',
             payload: 'vote an anecdote once',
@@ -44,13 +43,18 @@ describe('anecdote reducer', () => {
     });
 
     test('Add new anecdote success', () => {
-            const action = {
+        const initialState = {anecdotes: [{
+            id: 0,
+            content: 'test anecdote',
+            votes: 0
+        }]};
+        const action = {
             type: 'ADD_ANECDOTE',
             payload: 'add new anecdote',
             data: {
                 content: 'test new anecdote'
             }
-        }
+        };
 
         const state = initialState;
         deepFreeze(state);
@@ -82,5 +86,30 @@ describe('anecdote reducer', () => {
             });
     });
 
+    test('init anecdotes test', () => {
+        const initialState = [];
+        
+        const action = {
+            type: 'INIT_ANECDOTES',
+            paylod: 'init anecdotes from the server',
+            anecdotes: []
+        }
 
+        const state = initialState;
+        deepFreeze(state);
+        const newState = anecdoteReducer(state,action);
+
+        expect(newState).toEqual({anecdotes:[]});
+
+    });
+
+    test('init anecdotes action object test', () => {
+        const action = initAnecdotesAction();
+        
+        expect(action).toEqual({
+            type: 'INIT_ANECDOTES',
+            payload: 'init anecdotes from the server',
+            anecdotes: expect.any(Array)
+        });
+    });
 });
